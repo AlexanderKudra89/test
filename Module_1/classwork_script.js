687bb35df9943502d41ca1d6c4b842c8f86016f0
AlexanderKudra89 <sasha.asnl@gmail.com>
2019-07-28 20:47:01 +0300

    add files

---
+++ Module_1/classwork_script.js
@@ -0,0 +1,15 @@
+const userName = 'Bob';
+const userLastName = 'Dilan';
+const userLogin = 'Bob91';
+const userPassword = 'Dilan91';
+
+let isUserLoginValid = false;
+let isUserPasswordValid = false;
+
+while(!isUserLoginValid) {
+    const userInput = prompt('input your login');
+    if (userImput === userLogin) {
+      isUserLoginValid = true;
+      console.log('hello')
+    }
+}
